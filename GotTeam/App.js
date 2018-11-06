6018162b9a3273373f1bed7edbf036d3183622d9
MahmoudElsayad <elsayadi5700@gmail.com>
2018-11-06 20:04:13 +0200

    Solved Babel problem, and notifications are not working again

--- GotTeam/App.js
+++ GotTeam/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { StyleSheet, StatusBar, View, AsyncStorage } from 'react-native';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import firebase from '@firebase/app'
-import RNfirebase from 'react-native-firebase';
-import type { Notification, NotificationOpen } from 'react-native-firebase';
 import '@firebase/auth'
 import '@firebase/firestore'
 import { Content, ActionSheet, Root, Toast } from 'native-base';
@@ -33,104 +31,8 @@ class AuthLoader extends React.Component {
     this._bootstrapAsync();
   }
 
-  componentDidMount = async () => {
-    const enabled = await RNfirebase.messaging().hasPermission();
-    if (enabled) {
-      // user has permissions
-    } else {
-      // user doesn't have permission
-      try {
-        await RNfirebase.messaging().requestPermission();
-        // User has authorised
-      } catch (error) {
-        // User has rejected permissions
-        alert('No permission for notification');
-      }
-    }
-
-    const notificationOpen: NotificationOpen = await RNfirebase.notifications().getInitialNotification();
-    if (notificationOpen) {
-      // App was opened by a notification
-      // Get the action triggered by the notification being opened
-      const action = notificationOpen.action;
-      console.log(notificationOpen);
-      
-      // Get information about the notification that was opened
-      const notification: Notification = notificationOpen.notification;
-      if (notification.body !== undefined) {
-        alert(notification.body);
-      } else {
-        var seen = [];
-        alert(JSON.stringify(notification.data, function (key, val) {
-          if (val != null && typeof val == "object") {
-            if (seen.indexOf(val) >= 0) {
-              return;
-            }
-            seen.push(val);
-          }
-          return val;
-        }));
-
-        console.log(notification.body);
-        console.log(notification);
-      }
-      RNfirebase.notifications().removeDeliveredNotification(notification.notificationId);
-    }
-
-    const channel = new RNfirebase.notifications.Android.Channel('test-channel', 'Test Channel', RNfirebase.notifications.Android.Importance.Max)
-      .setDescription('My apps test channel');
-
-    // Create the channel
-    RNfirebase.notifications().android.createChannel(channel);
-
-    RNfirebase.messaging().subscribeToTopic('news1');
-
-    this.notificationDisplayedListener = RNfirebase.notifications().onNotificationDisplayed((notification: Notification) => {
-      // Process your notification as required
-      // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
-    });
-    this.notificationListener = RNfirebase.notifications().onNotification((notification: Notification) => {
-      // Process your notification as required
-      console.log('get Message');
-      console.log(notification);
-      notification
-        .android.setChannelId('test-channel')
-        .android.setSmallIcon('ic_launcher');
-      RNfirebase.notifications()
-        .displayNotification(notification);
-    
-    });
-    this.notificationOpenedListener = RNfirebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
-      // Get the action triggered by the notification being opened
-      const action = notificationOpen.action;
-      // Get information about the notification that was opened
-      const notification: Notification = notificationOpen.notification;
-      
-      if (notification.body !== undefined) {
-        alert(notification.body);
-       
-      } else {
-        var seen = [];
-        alert(JSON.stringify(notification.data, function (key, val) {
-          if (val != null && typeof val == "object") {
-            if (seen.indexOf(val) >= 0) {
-              return;
-            }
-            seen.push(val);
-          }
-          return val;
-        }));
-      }
-      RNfirebase.notifications().removeDeliveredNotification(notification.notificationId);
-    });
-  }
-
-
   componentWillUnmount() {
     ActionSheet.actionsheetInstance = null;
-    this.notificationDisplayedListener();
-    this.notificationListener();
-    this.notificationOpenedListener();
   }
 
   // Fetch the token from storage then navigate to our appropriate place
@@ -152,31 +54,33 @@ class AuthLoader extends React.Component {
   render() {
     return (
       <Root>
-      <View style={styles.container}>
-        {/* <PulseIndicator  color='#4caf50' size={100} /> */}
+        <View style={styles.container}>
+          {/* <PulseIndicator  color='#4caf50' size={100} /> */}
           <Spinner style={styles.spinner} size={60} type={'9CubeGrid'} color={'#19CFA0'} />
-        <StatusBar barStyle="default" />
-      </View>
+          <StatusBar barStyle="default" />
+        </View>
       </Root>
     );
   }
 }
 
 const TeamsTab = createStackNavigator({
-  Tabs: { screen: Tabs,
+  Tabs: {
+    screen: Tabs,
     navigationOptions: {
       header: null
-    } },
+    }
+  },
   AddTeams: { screen: AddTeams },
   CreateTeam: { screen: CreateTeam },
   TeamCreated: { screen: TeamCreated },
-  TeamInfo:{ screen: TeamInfo },
+  TeamInfo: { screen: TeamInfo },
   TeamOptions: { screen: TeamOptions },
   JoinTeams: { screen: JoinTeams },
   TeamMessage: { screen: TeamMessage },
   LocationPage: { screen: LocationPage },
   EditTeamInfo: { screen: EditTeamInfo },
-  Notifications: { screen: Notifications}
+  Notifications: { screen: Notifications }
 },
 );
 
--- GotTeam/components/Tabs/Screens/Events.js
+++ GotTeam/components/Tabs/Screens/Events.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import { StyleSheet, TouchableHighlight } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { NavigationBar, Title, Switch, TextInput, View, Row, Text, Subtitle, Divider, Caption, Button } from '@shoutem/ui';
+import { NavigationBar, Title, Switch, TextInput, View, Row, Text, Subtitle, Divider, Caption, Button, TouchableOpacity } from '@shoutem/ui';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 
 class Events extends Component {
 
     render() {
         return (
-            <View styleName='fill-parent' style={{ backgroundColor: '#fff'}}>
+            <View styleName='fill-parent' style={{ backgroundColor: '#fff' }}>
                 <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={['#0BC5B7', '#29D890']} style={styles.linearGradient}>
-                <NavigationBar
-                styleName='clear'
-                    // style={{
-                    //     container: {
-                    //         backgroundColor: '#4caf50'
-                    //     },
-                    // }}
-                    centerComponent={<Title style={{ fontSize: 20, paddingTop: 3, }}>Events</Title>}
-                />
+                    <NavigationBar
+                        styleName='clear'
+                        // style={{
+                        //     container: {
+                        //         backgroundColor: '#4caf50'
+                        //     },
+                        // }}
+                        leftComponent={
+                            <Title style={{ marginLeft: '25%', fontSize: 20, paddingTop: 3, }}>Events</Title>
+                        }
+                        rightComponent={
+                            <View style={{ flexDirection: 'row', marginRight: '15%', marginTop: '2%', width: '130%' }}>
+                                <TouchableOpacity>
+                                    <Icon style={{ marginLeft: '10%' }} name="bell" size={30} color="#fff" />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <Icon style={{ marginLeft: '10%' }} name="heart-o" size={30} color="#fff" />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <Icon style={{ marginLeft: '10%' }} name="calendar" size={30} color="#fff" />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <Icon style={{ marginLeft: '10%' }} name="plus-circle" size={30} color="#fff" />
+                                </TouchableOpacity>
+                            </View>
+                        }
+                    />
                 </LinearGradient>
             </View>
         );
